fb9f72bfdfdbbc3d95ec4bede1448600c8cf7545
Emilson Filho <emilsonfilhocontato@gmail.com>
2026-05-07 11:07:57 -0300

    feat(schema): subject entity

---
+++ app/schemas/subject_schema.py
@@ -0,0 +1,29 @@
+from typing import Annotated
+from pydantic import BaseModel, Field
+
+class SubjectBase(BaseModel):
+    name: Annotated[
+        str,
+        Field(
+            min_length=5,
+            max_length=60,
+            description="Nome da matéria."
+        )
+    ]
+
+class CreateSubjectRequest(SubjectBase):
+    pass
+
+class UpdateSubjectRequest(BaseModel):
+    name: Annotated[
+        str | None,
+        Field(
+            default=None,
+            min_length=5,
+            max_length=60,
+            description="Nome da matéria."
+        )
+    ]
+
+class SubjectResponse(SubjectBase):
+    id: int
