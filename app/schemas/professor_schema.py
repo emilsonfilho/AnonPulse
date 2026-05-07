fd32033765adb27844cc1d0bdfcfc96abdce25b5
Emilson Filho <emilsonfilhocontato@gmail.com>
2026-05-07 10:58:30 -0300

    feat(schema): professor entity

---
+++ app/schemas/professor_schema.py
@@ -0,0 +1,35 @@
+from typing import Annotated
+from pydantic import BaseModel, Field, EmailStr
+
+class ProfessorBase(BaseModel):
+    name: Annotated[
+        str,
+        Field(
+            min_length=3,
+            max_length=100,
+            description="Nome do professor."
+        )
+    ]
+    email: EmailStr = Field(
+        min_length=5,
+        max_length=100,
+        description="Email do professor."
+    )
+
+class CreateProfessorBase(ProfessorBase):
+    pass
+
+class UpdateProfessorBase(BaseModel):
+    name: Annotated[
+        str | None,
+        Field(
+            min_length=3,
+            max_length=100,
+            description="Nome do professor."
+        )
+    ]
+    email: EmailStr | None = Field(
+        min_length=5,
+        max_length=100,
+        description="Email do professor."
+    )
