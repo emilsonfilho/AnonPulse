58cb6978b388b9aed0181feb30eac03414ba4416
Emilson Filho <emilsonfilhocontato@gmail.com>
2026-05-11 13:58:54 -0300

    ref(SubjectService): following DRY principles

--- app/services/subject_service.py
+++ app/services/subject_service.py
@@ -3,28 +3,33 @@ from fastapi_pagination import Params, Page
 from app.schemas.subject_schema import CreateSubjectRequest, UpdateSubjectRequest, SubjectResponse
 from app.models import Subject
 from app.core.exceptions.custom_exceptions import SubjectAlreadyExistsExcepion, SubjectNotFoundException
+from app.core.mapper import Mapper
 from app.repositories.subject_repository import SubjectRepository
 
 class SubjectService:
     def __init__(self, repository: SubjectRepository) -> None:
         self.repository = repository
+
+    async def _get_subject_or_raise(self, code: str) -> Subject:
+        subject = await self.repository.get_by_code(code)
+
+        if not subject:
+            raise SubjectNotFoundException()
+
+        return subject
     
     async def list_subjects(self, params: Params) -> Page[SubjectResponse]:
         page = await self.repository.list_all(params)
 
         page.items = [
-            SubjectResponse.model_validate(subject) for subject in page
+            Mapper.to_response(subject, SubjectResponse) for subject in page
         ] 
 
         return page
 
     async def get_subject_by_code(self, code: str) -> SubjectResponse:
-        subject = await self.repository.get_by_code(code)
-
-        if not subject:
-            raise SubjectNotFoundException()
-        
-        return SubjectResponse.model_validate(subject)
+        subject = await self._get_subject_or_raise(code)
+        return Mapper.to_response(subject, SubjectResponse)
 
 
     async def create(self, data: CreateSubjectRequest) -> SubjectResponse:
@@ -38,25 +43,18 @@ class SubjectService:
         subject = Subject(**data.model_dump())
         new_subject = await self.repository.create(subject)
 
-        return SubjectResponse.model_validate(new_subject)
+        return Mapper.to_response(new_subject, SubjectResponse)
     
     async def update_subject(self, code: str, request: UpdateSubjectRequest) -> SubjectResponse:
-        subject_exists = await self.repository.get_by_code(code)
-
-        if not subject_exists:
-            raise SubjectNotFoundException()
+        self._get_subject_or_raise(code)
 
         updated_subject = await self.repository.update(
             code,
             request.model_dump(exclude_unset=True)
         )
 
-        return SubjectResponse.model_validate(updated_subject)
+        return Mapper.to_response(updated_subject, SubjectResponse)
     
     async def delete_suject(self, code: str) -> None:
-        subject_exists = await self.repository.get_by_code(code)
-
-        if not subject_exists:
-            raise SubjectNotFoundException()
-        
+        self._get_subject_or_raise(code) 
         self.repository.delete(code)
