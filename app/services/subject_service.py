da1a50b4fb05e71305160dbc204b0ebf743a25fa
Emilson Filho <emilsonfilhocontato@gmail.com>
2026-05-11 08:53:55 -0300

    feat(service): implementing SubjectService

--- app/services/subject_service.py
+++ app/services/subject_service.py
@@ -1,12 +1,25 @@
-from app.schemas.subject_schema import CreateSubjectRequest, SubjectResponse
+from fastapi_pagination import Params, Page
+
+from app.schemas.subject_schema import CreateSubjectRequest, UpdateSubjectRequest, SubjectResponse
 from app.models import Subject
-from app.core.exceptions.custom_exceptions import SubjectAlreadyExistsExcepion
+from app.core.exceptions.custom_exceptions import SubjectAlreadyExistsExcepion, SubjectNotFoundException
+from app.repositories.subject_repository import SubjectRepository
 
 class SubjectService:
-    def __init__(self, repository) -> None:
+    def __init__(self, repository: SubjectRepository) -> None:
         self.repository = repository
+    
+    async def list_subjects(self, params: Params) -> Page[SubjectResponse]:
+        return await self.repository.list_all() 
 
-    async def create_subject(self, data: CreateSubjectRequest):
+    async def get_subject_by_code(self, code: str) -> SubjectResponse:
+        subject = await self.repository.get_by_code(code)
+
+        if not subject:
+            raise SubjectNotFoundException()
+
+
+    async def create(self, data: CreateSubjectRequest) -> SubjectResponse:
         subject_exists = await self.repository.get_by_code(data.cod)
 
         if (subject_exists):
@@ -18,3 +31,24 @@ class SubjectService:
         new_subject = await self.repository.create(subject)
 
         return SubjectResponse.model_validate(new_subject)
+    
+    async def update_subject(self, code: str, request: UpdateSubjectRequest) -> SubjectResponse:
+        subject_exists = await self.repository.get_by_code(code)
+
+        if not subject_exists:
+            raise SubjectNotFoundException()
+
+        updated_subject = await self.repository.update(
+            code,
+            request.model_dump(exclude_unset=True)
+        )
+
+        return SubjectResponse.model_validate(updated_subject)
+    
+    async def delete_suject(self, code: str) -> None:
+        subject_exists = await self.repository.get_by_code(code)
+
+        if not subject_exists:
+            raise SubjectNotFoundException()
+        
+        self.repository.delete(code)
