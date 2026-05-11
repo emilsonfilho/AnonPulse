bae4446859c542f3563ac3b256a8ab5babfc174b
Emilson Filho <emilsonfilhocontato@gmail.com>
2026-05-11 08:53:03 -0300

    feat(exceptions): new exceptions for subject

--- app/core/exceptions/custom_exceptions.py
+++ app/core/exceptions/custom_exceptions.py
@@ -13,3 +13,9 @@ class SubjectAlreadyExistsExcepion(Exception):
     def __init__(self, message: str) -> None:
         super().__init__(message)
         self.message = message
+
+class SubjectNotFoundException(Exception):
+    def __init__(self) -> None:
+        message = "Disciplina não enconotrada."
+        super().__init__(message)
+        self.message = message
