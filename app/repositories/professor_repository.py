256027f98d85372c6ccdce26bfaed7cc6b8a06e9
Emilson Filho <emilsonfilhocontato@gmail.com>
2026-06-20 17:03:06 -0300

    ref(repository): implementing professor repository with pagination

--- app/repositories/professor_repository.py
+++ app/repositories/professor_repository.py
@@ -3,6 +3,6 @@ from app.repositories.base_repository import BaseRepository
 
 class ProfessorRepository(BaseRepository):
     """Repositorio para a entidade Professor."""
-    def __init__(self, session: AsyncSession) -> None:
+    def __init__(self) -> None:
         """Inicializa o repositório com a sessão do banco de dados."""
         super().__init__(model=Professor)
