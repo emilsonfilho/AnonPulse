76d26b5973d61986f727eedb4bed99890eb6abdf
Emilson Filho <emilsonfilhocontato@gmail.com>
2026-06-20 17:00:15 -0300

    feat(pagination): adding pagination class

---
+++ app/core/pagination/pagination.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclass
+from fastapi_pagination import Params
+
+@dataclass
+class Pagination:
+    skip: int
+    limit: int
+
+    @staticmethod
+    def from_params(params: Params):
+        return Pagination(
+            skip = (params.page - 1) * params.size,
+            limit = params.size
+        )
